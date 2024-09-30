17bf71dbe70ffd8f8349368ea1afcb174fe1cc69
RicaLucino <20211531@nbsc.edu.ph>
2024-10-01 01:32:42 +0800

    added about.php its css components with info. 1 image was added for user profile and few user dashboard components are added/edited

---
+++ home/about.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About Us</title>
+    <link rel="stylesheet" href="../css/about_style.css">
+</head>
+
+<body>
+    <!-- Exit button -->
+    <li><a href="home.html" class="exit-btn"> Back </a></li>
+
+    <!-- Hero Section -->
+    <section class="hero">
+        <div class="container hero-content">
+            <div class="text-section">
+                <h1>About Us </h1>
+                <p> Connecting owners and animals to make a difference. By fostering relationships and raising awareness, 
+                    we aim to save more animal lives through rescue, adoption, and building a community.  </p>
+                <div class="button-group">
+                    <button class="btn-primary">Learn more</button>
+                </div>
+            </div>
+            <div class="image-section">
+                <img src="" alt="cutees">
+            </div>
+        </div>
+    </section>
+
+    <!-- Information Cards -->
+    <section class="info-cards">
+        <div class="container">
+        <h3>Contact Us</h3>
+
+            <div class="card">
+                <h3>Email</h3>
+                <li>abcdef@gmail.com</li>
+                <li>ghijkl@gmail.com</li>
+                <li>mnopqr@gmail.com</li>
+            </div>
+
+            <div class="card">
+                <h3>Phone</h3>
+                <li>+6391196640284</li>
+                <li>+6390382628938</li>
+                <li>+6398475940339</li>
+                
+            </div>
+
+            <div class="card">
+                <h3>SocMed</h3>
+                <li>abcdef@fb.com</li>
+                <li>minari@insta.com</li>
+                <li>rosies_@x.com</li>
+            </div>
+        </div>
+    </section>
+</body>
+</html>
